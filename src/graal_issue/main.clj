56848b972c22d5eea6de6768a72e90c4c30d327a
Bruno Bonacci <bruno.bonacci@gmail.com>
2023-08-08 17:33:50 +0100

    testing clj-yaml

--- src/graal_issue/main.clj
+++ src/graal_issue/main.clj
@@ -1,8 +1,16 @@
 (ns graal-issue.main
   (:gen-class)
-  (:require [com.brunobonacci.oneconfig :refer [configure]]))
+  (:require [clj-yaml.core :as yaml]))
+
+(comment
+  (defn -main [& _args]
+    (println "configuration: " (configure {:key "test" :version "0.0.1" :env "local"}))))
 
 
 
-(defn -main [& _args]
-  (println "configuration: " (configure {:key "test" :version "0.0.1" :env "local"})))
+(defn -main [& args]
+  (println
+    (yaml/generate-string
+      [{:name "John Smith", :age 33}
+       {:name "Mary Smith", :age 27}]))
+  )
