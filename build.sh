2bf7d2d53f48d4505f8089eaa35d020e8cbbbb55
Brandon Stubbs <brandon@bos-solutions.co.uk>
2023-08-08 12:36:40 -0400

    testing clj-yaml (#1)
    Co-authored-by: Bruno Bonacci <bruno.bonacci@gmail.com>

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -xe
+# build uberjar
+lein do clean, uberjar
+
+# build native-image (snakeyaml error)
+native-image \
+  -H:+AllowIncompleteClasspath \
+  --verbose \
+  --no-fallback \
+  --enable-url-protocols=http,https \
+  --report-unsupported-elements-at-runtime \
+  --no-server \
+  --features=clj_easy.graal_build_time.InitClojureClasses \
+  -jar target/graal-issue-0.0.1-standalone.jar \
+  target/graal-issue
--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :main graal-issue.main
 
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [com.brunobonacci/oneconfig "0.22.0"]
+                 #_[com.brunobonacci/oneconfig "0.22.0"]
+                 [clj-commons/clj-yaml "1.0.26"]
                  [com.github.clj-easy/graal-build-time "1.0.5"]])
